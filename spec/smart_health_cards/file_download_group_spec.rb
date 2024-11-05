c2a179cf42af4d2a8e09e73ea84d272c924b6f01
Brian Meshell <bmeshell@mitre.org>
2024-11-05 09:35:37 -0500

    refactoring for easier maintainability. Moved payload verification unit tests to a new file. Added 'format documentation' flag to show output from unit tests in all spec files.

--- spec/smart_health_cards/file_download_group_spec.rb
+++ spec/smart_health_cards/file_download_group_spec.rb
@@ -59,37 +59,7 @@ RSpec.describe SmartHealthCards::FileDownloadGroup do
     end
   end
 
-  describe 'health_card_payload_test' do
-    let(:test) { group.tests[4] }
-    let(:url) { 'http://example.com/hc' }
-
-    #todo: for a more thorough test, consider setting credential_strings to a string of comma-separated of several VCs instead of a single VC string 
-    credential_strings = 'eyJ6aXAiOiJERUYiLCJhbGciOiJFUzI1NiIsImtpZCI6IjRIVWIyYXJ2aFRTWHNzRW9NczJHNVRvRHBzWXZzajdoNXdUXzN6TkV0dWcifQ.hVLLjtQwEPyX5pqHk5nMI0fggoQAwcIFzcFxOhMjx45sJ2JY5d9pJ7tktLuz5BLZrq6uqu57kM5BCa33fZmmygiuWuN8uWGMQQS6aqDM9hu22xd5nkUwCijvwV96hPLnXOaoznXc-ha58m0iuK3dm-UQhwPR3MYJM8o6O76KkV03aPmHe2k0nCIQFmvUXnL1bah-ofBBUtNK-wOtC5gStglLMiINt28HXStcZYMwSlFVQEZARPZCXohhUOq7VQSw6MxgBZYhgsdDINC8wwXLO6moDD5y62aesxxRh0y-ctEiNYDTRForSWbecx_6ZsddFrMszo9XtHeLpi_kjqTANEUvKsmeKHGe-8HNZrpeoccQ88iFkBrfmXrGCFNLfZ71uovz2K2DbtU-MfachnxSJ-tUjL-JQMyVkLMDTKcpgv5BFZFZbNCiDt2v8yGQEWKw81PweSe7hSLfxhkju0SrjP80dBXaEEK-2Ra7_fMEPlxP-VYM-a0YGqm5-ufgVe_KSC2C-9VwwYrtId4vpt26VLdNY9OEFRpf8pwV8yzUME-CV4r-xNH7_whz2nRYJ1I3JnUkYJ3Hjm0OBWPHReCT4D5XDu34mNvp2fvD_k_0_QU.-jNkrXCHlq75fLCGvD8_7eF4iQ-XYQT7uZyiZ1Fqa33-ZQA1-aVEk519JZYGMDdJpO-mVqIC20Xh9sBsD8COzg'
-
-    it 'passes if JWS payload is structured correctly' do
-      
-      result = run(test, { file_download_url: url, url: url, credential_strings: credential_strings })
-
-      payloads = JSON.parse(result.output_json)
-      payloads.each do |payload|
-        value = JSON.parse(payload['value'])
-
-        expect(value['nbf']).to be_a_kind_of(Integer)
-
-        #TODO: work in progress - more tests will go here
-        
-        #binding.pry
-
-      end
-      
-    end
-
-    
-    
-    
-
-
-  end
+  
 
 
 
