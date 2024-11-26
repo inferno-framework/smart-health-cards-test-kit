d858bebc2ea7a28085258e32e10d91b5cf929c5d
Brian Meshell <bmeshell@mitre.org>
2024-11-26 10:41:41 -0500

    created FHIR validation test and corresponding unit test (wip). Need url for bundle structure definition.

--- lib/smart_health_cards/shc_fhir_validation.rb
+++ lib/smart_health_cards/shc_fhir_validation.rb
@@ -3,12 +3,47 @@ module SmartHealthCards
     id :shc_fhir_validation_test
     title 'Smart Health Card payloads conform to the correct Bundle Profiles' #TODO: update title with specific bundle type
     input :credential_strings
-    output :bundles
+    output :fhir_bundles
 
     run do
 
-      #TODO complete stub test
-      
+      skip_if credential_strings.blank?, 'No Verifiable Credentials received'
+
+      credential_strings.split(',').each do |credential|
+        raw_payload = HealthCards::JWS.from_jws(credential).payload
+        assert raw_payload&.length&.positive?, 'No payload found'
+
+        decompressed_payload =
+          begin
+            Zlib::Inflate.new(-Zlib::MAX_WBITS).inflate(raw_payload)
+          rescue Zlib::DataError
+            assert false, 'Payload compression error. Unable to inflate payload.'
+          end
+
+        payload = JSON.parse(decompressed_payload)
+        vc = payload['vc']
+        assert vc.is_a?(Hash), "Expected 'vc' claim to be a JSON object, but found #{vc.class}"
+
+        subject = vc['credentialSubject']
+        assert subject.is_a?(Hash), "Expected 'vc.credentialSubject' to be a JSON object, but found #{subject.class}"
+
+        raw_bundle = subject['fhirBundle']
+        assert raw_bundle.is_a?(Hash), "Expected 'vc.fhirBundle' to be a JSON object, but found #{raw_bundle.class}"
+
+        bundle = FHIR::Bundle.new(raw_bundle)
+
+        #binding.pry
+
+        #TODO: need url for SHC bundle (existing url was copied from vaccination test kit)
+        warning do
+          assert_valid_resource(
+            resource: bundle,
+            profile_url: 'http://hl7.org/fhir/uv/smarthealthcards-vaccination/StructureDefinition/vaccination-credential-bundle-dm'
+          )
+        end
+
+      end
+      #TODO: populate output variable fhir_bundles
     end
   end
 end
--- spec/smart_health_cards/shc_fhir_validation_spec.rb
+++ spec/smart_health_cards/shc_fhir_validation_spec.rb
@@ -19,10 +19,32 @@ RSpec.describe SmartHealthCards::SHCHeaderVerification do
 
   describe 'health_card_fhir_validation_test' do
 
-    #TODO: complete stub unit test
-    it 'passes. That\'s it. It just passes' do 
-      expect(5).to be > 4 #this is only a test
+    let(:test) { group.tests[6] } #TODO: is there a way to identify the tests without the hard-coded array index?
+    let(:url) { 'http://example.com/hc' }
+
+    #TODO: update text with specific bundle type
+    it 'passes if the JWS payload conforms to the correct FHIR Bundle' do 
+      credential_strings = 'eyJ6aXAiOiJERUYiLCJhbGciOiJFUzI1NiIsImtpZCI6IjRIVWIyYXJ2aFRTWHNzRW9NczJHNVRvRHBzWXZzajdoNXdUXzN6TkV0dWcifQ.hVLLjtQwEPyX5pqHk5nMI0fggoQAwcIFzcFxOhMjx45sJ2JY5d9pJ7tktLuz5BLZrq6uqu57kM5BCa33fZmmygiuWuN8uWGMQQS6aqDM9hu22xd5nkUwCijvwV96hPLnXOaoznXc-ha58m0iuK3dm-UQhwPR3MYJM8o6O76KkV03aPmHe2k0nCIQFmvUXnL1bah-ofBBUtNK-wOtC5gStglLMiINt28HXStcZYMwSlFVQEZARPZCXohhUOq7VQSw6MxgBZYhgsdDINC8wwXLO6moDD5y62aesxxRh0y-ctEiNYDTRForSWbecx_6ZsddFrMszo9XtHeLpi_kjqTANEUvKsmeKHGe-8HNZrpeoccQ88iFkBrfmXrGCFNLfZ71uovz2K2DbtU-MfachnxSJ-tUjL-JQMyVkLMDTKcpgv5BFZFZbNCiDt2v8yGQEWKw81PweSe7hSLfxhkju0SrjP80dBXaEEK-2Ra7_fMEPlxP-VYM-a0YGqm5-ufgVe_KSC2C-9VwwYrtId4vpt26VLdNY9OEFRpf8pwV8yzUME-CV4r-xNH7_whz2nRYJ1I3JnUkYJ3Hjm0OBWPHReCT4D5XDu34mNvp2fvD_k_0_QU.-jNkrXCHlq75fLCGvD8_7eF4iQ-XYQT7uZyiZ1Fqa33-ZQA1-aVEk519JZYGMDdJpO-mVqIC20Xh9sBsD8COzg'
+      
+      stub_request(:post, "https://example.com/validatorapi/validate").
+         with(
+           body: "{\"cliContext\":{\"sv\":\"4.0.1\",\"doNative\":false,\"extensions\":[\"any\"],\"disableDefaultResourceFetcher\":true,\"profiles\":[\"http://hl7.org/fhir/uv/smarthealthcards-vaccination/StructureDefinition/vaccination-credential-bundle-dm\"]},\"filesToValidate\":[{\"fileName\":\"Bundle/.json\",\"fileContent\":\"{\\\"type\\\":\\\"collection\\\",\\\"entry\\\":[{\\\"fullUrl\\\":\\\"resource:0\\\",\\\"resource\\\":{\\\"name\\\":[{\\\"family\\\":\\\"Larson\\\",\\\"given\\\":[\\\"Rachelle\\\"]}],\\\"birthDate\\\":\\\"1961-01-29\\\",\\\"resourceType\\\":\\\"Patient\\\"}},{\\\"fullUrl\\\":\\\"resource:1\\\",\\\"resource\\\":{\\\"status\\\":\\\"completed\\\",\\\"vaccineCode\\\":{\\\"coding\\\":[{\\\"system\\\":\\\"http://hl7.org/fhir/sid/cvx\\\",\\\"code\\\":\\\"208\\\"}]},\\\"patient\\\":{\\\"reference\\\":\\\"resource:0\\\"},\\\"occurrenceDateTime\\\":\\\"2024-10-01\\\",\\\"lotNumber\\\":\\\"1234567\\\",\\\"resourceType\\\":\\\"Immunization\\\"}},{\\\"fullUrl\\\":\\\"resource:2\\\",\\\"resource\\\":{\\\"status\\\":\\\"final\\\",\\\"code\\\":{\\\"coding\\\":[{\\\"system\\\":\\\"http://loinc.org\\\",\\\"code\\\":\\\"50548-7\\\"}]},\\\"subject\\\":{\\\"reference\\\":\\\"resource:0\\\"},\\\"effectiveDateTime\\\":\\\"2024-10-15\\\",\\\"valueCodeableConcept\\\":{\\\"coding\\\":[{\\\"system\\\":\\\"http://snomed.info/sct\\\",\\\"code\\\":\\\"260385009\\\"}]},\\\"resourceType\\\":\\\"Observation\\\"}}],\\\"resourceType\\\":\\\"Bundle\\\"}\",\"fileType\":\"json\"}],\"sessionId\":null}",
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'Content-Type'=>'application/json',
+          'User-Agent'=>'Faraday v1.10.3'
+           }).
+         to_return(status: 200, body: "", headers: {})
+      
+      result = run(test, { file_download_url: url, url: url, credential_strings: credential_strings })
+      #binding.pry
+      expect(result.result).to eq('pass')
     end
+
+    #TODO: unit test for a comma-separated list of VC strings
+
+    #TODO: unit test that is expected to throw an error for invalid VC string
 
   end
 
