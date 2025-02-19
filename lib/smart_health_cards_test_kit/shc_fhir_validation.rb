a8d51be912f9f5888a5c2ebe2fd8df80d0055169
Brian Meshell <bmeshell@mitre.org>
2025-02-19 10:51:05 -0500

    parse json to get the correct input, wip

--- lib/smart_health_cards_test_kit/shc_fhir_validation.rb
+++ lib/smart_health_cards_test_kit/shc_fhir_validation.rb
@@ -7,33 +7,15 @@ module SmartHealthCardsTestKit
     description %(
       SMART Health Card payload SHALL be a valid FHIR Bundle resource
     )
-    input :credential_strings
-    output :fhir_bundles
+    input :fhir_bundles
 
     run do
+      
+      #skip_if fhir_bundles.blank?, 'No FHIR bundles received'
+      jsonArray = JSON.parse(fhir_bundles)
+      jsonArray.each { |json| assert_valid_resource(resource: FHIR::Bundle.new(json)) }
+      #fhir_bundles.each { |bundle| assert_valid_resource(resource: bundle) }
 
-      skip_if credential_strings.blank?, 'No Verifiable Credentials received'
-      bundle_array = []
-
-      credential_strings.split(',').each do |credential|
-        jws = SmartHealthCardsTestKit::Utils::JWS.from_jws(credential)
-        payload = payload_from_jws(jws)
-
-        vc = payload['vc']
-        assert vc.is_a?(Hash), "Expected 'vc' claim to be a JSON object, but found #{vc.class}"
-
-        subject = vc['credentialSubject']
-        assert subject.is_a?(Hash), "Expected 'vc.credentialSubject' to be a JSON object, but found #{subject.class}"
-
-        raw_bundle = subject['fhirBundle']
-        assert raw_bundle.is_a?(Hash), "Expected 'vc.fhirBundle' to be a JSON object, but found #{raw_bundle.class}"
-
-        bundle = FHIR::Bundle.new(raw_bundle)
-        assert_valid_resource(resource: bundle)
-        bundle_array.append(bundle)
-
-      end
-      output fhir_bundles: bundle_array
     end
   end
 end
