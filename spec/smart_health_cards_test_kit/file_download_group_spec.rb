4f69b27eb71e2e44e0daefb8b78398127b53b15c
Shaumik-Ashraf <shaumikashraf@mitre.org>
2025-07-08 15:59:52 -0400

    rm redundant methods in rspec

--- spec/smart_health_cards_test_kit/file_download_group_spec.rb
+++ spec/smart_health_cards_test_kit/file_download_group_spec.rb
@@ -1,24 +1,9 @@
 RSpec.describe SmartHealthCardsTestKit::FileDownloadGroup do
-  let(:suite) { Inferno::Repositories::TestSuites.new.find('smart_health_cards') }
-  let(:session_data_repo) { Inferno::Repositories::SessionData.new }
-  let(:runner) { Inferno::TestRunner.new(test_session: test_session, test_run: test_run) }
-  let(:test_session) do
-    Inferno::Repositories::TestSessions.new.create(test_suite_id: suite.id)
-  end
+  let(:suite_id) { 'smart_health_cards' }
   let(:request_repo) { Inferno::Repositories::Requests.new }
-  let(:group) { suite.groups.find { |g| g.id.include?('shc_file_download_group')} }
-
-  def run(runnable, inputs = {})
-    test_run_params = { test_session_id: test_session.id }.merge(runnable.reference_hash)
-    test_run = Inferno::Repositories::TestRuns.new.create(test_run_params)
-    inputs.each do |name, value|
-      session_data_repo.save(test_session_id: test_session.id, name: name, value: value, type: 'text')
-    end
-    Inferno::TestRunner.new(test_session: test_session, test_run: test_run).run(runnable)
-  end
 
   describe 'health_card_download_test' do
-    let(:test) { group.tests.find { |t| t.id.include?('health_card_download_test')} }
+    let(:test) { find_test suite, 'health_card_download_test' }
     let(:url) { 'http://example.com/hc' }
 
     it 'passes if valid json is downloaded' do
@@ -26,7 +11,7 @@ RSpec.describe SmartHealthCardsTestKit::FileDownloadGroup do
         stub_request(:get, url)
           .to_return(status: 200, body: { abc: 'def' }.to_json)
 
-      result = run(test, { file_download_url: url, url: url })
+      result = run(test, { file_download_url: url })
 
       expect(stubbed_request).to have_been_made.once
       expect(result.result).to eq('pass')
@@ -37,7 +22,7 @@ RSpec.describe SmartHealthCardsTestKit::FileDownloadGroup do
         stub_request(:get, url)
           .to_return(status: 500, body: { abc: 'def' }.to_json)
 
-        result = run(test, { file_download_url: url, url: url })
+        result = run(test, { file_download_url: url })
 
       expect(result.result).to eq('fail')
       expect(result.result_message).to match(/200/)
@@ -48,7 +33,7 @@ RSpec.describe SmartHealthCardsTestKit::FileDownloadGroup do
         stub_request(:get, url)
           .to_return(status: 200, body: 'def')
 
-      result = run(test, { file_download_url: url, url: url })
+      result = run(test, { file_download_url: url })
 
       expect(result.result).to eq('fail')
       expect(result.result_message).to match(/JSON/)
--- spec/smart_health_cards_test_kit/shc_fhir_validation_spec.rb
+++ spec/smart_health_cards_test_kit/shc_fhir_validation_spec.rb
@@ -1,32 +1,15 @@
 RSpec.describe SmartHealthCardsTestKit::SHCFHIRValidation do
-  let(:suite) { Inferno::Repositories::TestSuites.new.find('smart_health_cards') }
-  let(:session_data_repo) { Inferno::Repositories::SessionData.new }
-  let(:runner) { Inferno::TestRunner.new(test_session: test_session, test_run: test_run) }
-  let(:test_session) do
-    Inferno::Repositories::TestSessions.new.create(test_suite_id: suite.id)
-  end
-  let(:request_repo) { Inferno::Repositories::Requests.new }
-  let(:group) { suite.groups.first }
-
-  def run(runnable, inputs = {})
-    test_run_params = { test_session_id: test_session.id }.merge(runnable.reference_hash)
-    test_run = Inferno::Repositories::TestRuns.new.create(test_run_params)
-    inputs.each do |name, value|
-      session_data_repo.save(test_session_id: test_session.id, name: name, value: value, type: 'text')
-    end
-    Inferno::TestRunner.new(test_session: test_session, test_run: test_run).run(runnable)
-  end
+  let(:suite_id) { 'smart_health_cards' }
 
   describe 'health_card_fhir_validation_test' do
-
-    let(:test) { group.tests.find { |t| t.id.include?('shc_fhir_validation_test')} }
+    let(:test) { find_test suite, 'shc_fhir_validation_test' }
     let(:url) { 'http://example.com/hc' }
     let(:operation_outcome_success) do
       {
         outcomes: [{
           issues: []
         }],
-        sessionId: 'b8cf5547-1dc7-4714-a797-dc2347b93fe2'
+        sessionId: test_session.id
       }
     end
     let(:fhir_bundle_corrina_rowe) do
@@ -71,7 +54,7 @@ RSpec.describe SmartHealthCardsTestKit::SHCFHIRValidation do
       )
     end
 
-    let (:fhir_bundle_deanne_gleichner) do
+    let(:fhir_bundle_deanne_gleichner) do
       FHIR::Bundle.new(
         type: 'collection',
         entry: [
@@ -116,13 +99,13 @@ RSpec.describe SmartHealthCardsTestKit::SHCFHIRValidation do
     end
 
     before do
-      stub_request(:post, "https://example.com/validatorapi/validate")
+      stub_request(:post, validation_url)
         .to_return(status: 200, body: operation_outcome_success.to_json)
     end
 
     it 'passes if the input is an array with a single bundle conforms to the FHIR Bundle profile' do
       fhir_bundles = [ fhir_bundle_corrina_rowe ].to_json
-      result = run(test, { file_download_url: url, url: url, fhir_bundles: fhir_bundles})
+      result = run(test, { file_download_url: url, fhir_bundles: fhir_bundles })
       expect(result.result).to eq('pass')
     end
 
@@ -131,14 +114,13 @@ RSpec.describe SmartHealthCardsTestKit::SHCFHIRValidation do
         fhir_bundle_corrina_rowe,
         fhir_bundle_deanne_gleichner
     ].to_json
-      result = run(test, { file_download_url: url, url: url, fhir_bundles: fhir_bundles})
+      result = run(test, { file_download_url: url, fhir_bundles: fhir_bundles })
       expect(result.result).to eq('pass')
     end
 
     it 'skips if the no FHIR bundles received' do
-      result = run(test, { file_download_url: url, url: url, fhir_bundles: [].to_json})
+      result = run(test, { file_download_url: url, fhir_bundles: [].to_json })
       expect(result.result).to eq('skip')
     end
-
   end
 end
